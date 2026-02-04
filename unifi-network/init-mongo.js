7f7c76bf73c2f16a96cf27627a2ca8908d135642
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2026-02-04 21:11:44 +0530

    fix: unifi stack auth issue

--- unifi-network/init-mongo.js
+++ unifi-network/init-mongo.js
@@ -1,2 +1,14 @@
-db.getSiblingDB("unifi").createUser({user: "unifi", pwd: "viQN069G7nf17ZITbbeM", roles: [{role: "dbOwner", db: "unifi"}]});
-db.getSiblingDB("unifi_stat").createUser({user: "unifi", pwd: "viQN069G7nf17ZITbbeM", roles: [{role: "dbOwner", db: "unifi_stat"}]});
+db = db.getSiblingDB("admin");
+
+db.system.users.deleteOne({ user: "unifi" });
+
+db.createUser({
+  user: "unifi",
+  pwd:  "viQN069G7nf17ZITbbeM",
+  roles: [
+    { role: "dbOwner", db: "unifi" },
+    { role: "dbOwner", db: "unifi_stat" },
+    { role: "dbOwner", db: "unifi_audit" },
+    { role: "dbOwner", db: "unifi_alarm" }
+  ]
+});
